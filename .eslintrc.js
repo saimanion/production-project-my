3a4c1fe97f3b8dff76115f73eb13dd5c5531d5cf
saimanion <97195306+saimanion@users.noreply.github.com>
2024-10-03 16:05:01 +0200

    added jest

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,6 +4,7 @@ module.exports = {
   env: {
     browser: true,
     es2021: true,
+    jest: true,
   },
   extends: [
     'plugin:react/recommended',
---
+++ src/shared/lib/ClassNames/ClassNames.test.ts
@@ -0,0 +1,27 @@
+import { ClassNames } from './ClassNames';
+
+describe('ClassNames', () => {
+  test('with only first param', () => {
+    expect(ClassNames('someClass')).toBe('someClass');
+  });
+  test('with additional params', () => {
+    const expected = 'someClass class1 class2';
+    expect(ClassNames('someClass', {}, ['class1', 'class2'])).toBe(expected);
+  });
+  test('with mods', () => {
+    const expected = 'someClass class1 class2 endClass';
+    expect(ClassNames('someClass', { endClass: true, class4: false }, ['class1', 'class2'])).toBe(expected);
+  });
+  test('with all mods true', () => {
+    const expected = 'someClass class1 class2 endClass class4';
+    expect(ClassNames('someClass', { endClass: true, class4: true }, ['class1', 'class2'])).toBe(expected);
+  });
+  test('with all mods false', () => {
+    const expected = 'someClass class1 class2';
+    expect(ClassNames('someClass', { endClass: false, class4: false }, ['class1', 'class2'])).toBe(expected);
+  });
+  test('with undefined mods', () => {
+    const expected = 'someClass class1 class2';
+    expect(ClassNames('someClass', { endClass: false, class4: undefined }, ['class1', 'class2'])).toBe(expected);
+  });
+});
