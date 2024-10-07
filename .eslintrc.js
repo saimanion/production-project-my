fe68d2fa93f4a510ce77de092706c96bde4e94f8
saimanion <97195306+saimanion@users.noreply.github.com>
2024-10-07 17:27:00 +0200

    worked storybook

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,30 +2,24 @@ module.exports = {
   env: {
     browser: true,
     es2021: true,
-    jest: true,
+    jest: true
   },
-  extends: [
-    'plugin:react/recommended',
-    'airbnb',
-    'plugin:i18next/recommended',
-  ],
+  extends: ['plugin:react/recommended', 'airbnb', 'plugin:i18next/recommended', 'plugin:storybook/recommended'],
   parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaFeatures: {
-      jsx: true,
+      jsx: true
     },
     ecmaVersion: 'latest',
-    sourceType: 'module',
+    sourceType: 'module'
   },
-  plugins: [
-    'react',
-    '@typescript-eslint',
-    'i18next',
-  ],
+  plugins: ['react', '@typescript-eslint', 'i18next'],
   rules: {
     indent: [2, 2],
     'react/jsx-indent': [2, 2],
-    'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx', 'tsx'] }],
+    'react/jsx-filename-extension': [1, {
+      extensions: ['.js', '.jsx', 'tsx']
+    }],
     'import/no-unresolved': 0,
     'import/prefer-default-export': 0,
     'no-unused-vars': 1,
@@ -37,23 +31,22 @@ module.exports = {
     'import/extensions': 'off',
     'import/no-extraneous-dependencies': 0,
     'no-underscore-dangle': 0,
-    'i18next/no-literal-string': ['error', { markupOnly: true, ignoreAttribute: [' data-testid'] }],
-    'max-len': [
-      'error', {
-        ignoreComments: true, code: 120,
-      },
-    ],
+    'i18next/no-literal-string': ['error', {
+      markupOnly: true,
+      ignoreAttribute: [' data-testid']
+    }],
+    'max-len': ['error', {
+      ignoreComments: true,
+      code: 120
+    }]
   },
   globals: {
-    __IS_DEV__: true,
+    __IS_DEV__: true
   },
-  overrides: [
-    {
-      files: ['**/src/**/*.test.{ts, tsx}'],
-      rules:
-        {
-          'i18next/no-literal-string': 'off',
-        },
-    },
-  ],
+  overrides: [{
+    files: ['**/src/**/*.test.{ts, tsx}'],
+    rules: {
+      'i18next/no-literal-string': 'off'
+    }
+  }]
 };
